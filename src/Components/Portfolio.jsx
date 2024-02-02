1680883fdab14fa491563370e0796130cb05db37
Saidikrom <yusupovsaidikrom9@gmail.com>
2024-02-02 10:36:40 +0500

    changed all info and sources

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/mac.jpeg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "mac in dark room";
 
 /**
  * Project list
@@ -31,29 +31,18 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Optimedia",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      " I developed mobile application called Optimedia by working in collaboration with a Backend and a Flutter developers.",
+    url: "https://play.google.com/store/apps/details?id=uz.optimedia.optimedia",
   },
   {
-    title: "Web Development for Beginners",
+    title: "SOFF",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "SOFF is my stratup project. SOFF is e-commerence app for furniture market. I develop this app using Flutter collabrote with Unity",
+    url: "https://t.me/soff_company",
   },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
-  },
+  
 ];
 
 const Portfolio = () => {
